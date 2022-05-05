21b0189d98d640c845009ef621d2e0ff49458428
maybemarhs <maybemar.hernandez@gmail.com>
2022-05-05 17:36:14 -0500

    Finalizing interface design

--- lib/screens/tasks_screens.dart
+++ lib/screens/tasks_screens.dart
@@ -6,28 +6,52 @@ class TaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black45,
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
       backgroundColor: Colors.black26,
-      body:SafeArea(
-        minimum: const EdgeInsets.all(30.0),
-        child: Column(
-          children: const <Widget>[
-            CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 30.0,
-                child: Icon(Icons.list,
-                  color: Colors.black45,
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+          padding: EdgeInsets.only(left: 30.0, top:60.0, right:30.0,bottom: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:  <Widget>[
+              CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(Icons.list,
+                    color: Colors.black45,
+                  ),
+              ),
+              SizedBox(height:20.0),
+              Text('ToDoList',
+                style: TextStyle(color:Colors.white,
+                fontSize: 30.0,
+                fontWeight: FontWeight.w700,
                 ),
-            ),
-            SizedBox(height:20.0),
-            Text('ToDoList',
-              style: TextStyle(color:Colors.white,
-              fontSize: 30.0,
-              fontWeight: FontWeight.w700,
+              ),
+              Text('12 Tasks',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0),topRight: Radius.circular(20.0),)
               ),
             ),
-          ],
-        ),
-      )
-    );
+          )
+      ]),
+      );
   }
 }
